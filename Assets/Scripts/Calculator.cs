ef564fa5b8ef6d46fac4c5770994e6a297afb759
Roydaaan <onibaku1@inbox.ru>
2024-10-10 14:22:42 +0300

    Update Script

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -2,28 +2,25 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Data;
+using UnityEngine.Serialization;
 
 public class Calculator : MonoBehaviour
 {
-    [SerializeField] private TMP_Text Text1;
-    [SerializeField] private string calc1;
-    [SerializeField] private string x1;
+    [SerializeField] private TMP_Text display;
+    [SerializeField] private string calc;
+    [SerializeField] private string x;
     [SerializeField] private Button button1;
 
     private void Awake() => button1.onClick.AddListener(OnClickButton);
 
-    private void OnClickButton() => Text1.text += calc1;
-    
+    private void OnClickButton() => display.text += calc;
     
     public void OnClickEquals()
     {
         DataTable dt = new DataTable();
-        x1 = (dt.Compute(Text1.text, "")).ToString();
-        Text1.text = x1;
+        x = (dt.Compute(display.text, "")).ToString();
+        display.text = x;
     }
-
-    public void OnClickC()
-    {
-        Text1.text = "";
-    }
+    
+    public void OnClickC() => display.text = "";
 }
